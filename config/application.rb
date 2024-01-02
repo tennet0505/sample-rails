725684e202c96d7057a9963892d4ecd61b75c451
Oleg Ten <tennet0505@gmail.com>
2024-01-02 22:27:39 +0100

    add logs

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'rails_loki_exporter_dev'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +19,16 @@ module SampleRails2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.after_initialize do
+      config_file_path = File.join(Rails.root, 'config', 'config.yml')
+      logger = RailsLokiExporterDev.create_logger(config_file_path)
+      Rails.logger = logger
+      Rails.logger.warn("[WARN] This is a warning message from the controller.")
+      Rails.logger.error("[ERROR] This is an error message from the controller.")
+      Rails.logger.fatal("[FATAL] This is a fatal error message from the controller.")
+      Rails.logger.info("This is an info message from the controller.")
+      Rails.logger.info("This is an info message from the controller.")
+      Rails.logger.info("This is an info message from the controller.")
+    end
   end
 end
