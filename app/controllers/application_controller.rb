7e92cdd7496a912379228d5b53ed0247f785f738
Oleg Ten <tennet0505@gmail.com>
2024-01-02 23:07:09 +0100

    log test controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-    def log_messages
-        Rails.logger.debug("[DEBUG] This is a debug message from the controller.")
-        Rails.logger.info("[INFO] This is an info message from the controller.")
-        Rails.logger.warn("[WARN] This is a warning message from the controller.")
-        Rails.logger.error("[ERROR] This is an error message from the controller.")
-        Rails.logger.fatal("[FATAL] This is a fatal error message from the controller.")
-        Rails.logger.info("This is an info message from the controller.")
-        render plain: "Log messages have been generated. Check the logs for details."
-    end
 end
