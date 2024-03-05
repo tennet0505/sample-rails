a755dd14b07453cdf37f31798f6e8498e3a2a0c4
Oleg Ten <tennet0505@gmail.com>
2024-03-06 00:46:52 +0100

    fix name

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -5,5 +5,4 @@ Pyroscope.configure do |config|
     config.server_address = "https://profiles-prod-001.grafana.net"
     config.basic_auth_username='786746'
     config.basic_auth_password='glc_eyJvIjoiOTg3NzQ5IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZXRva2Vud3JpdGVyZWFkIiwiayI6ImxDQTI0NGxORjFRclMxRzVBODhLNjJsUiIsIm0iOnsiciI6InByb2QtdXMtZWFzdC0wIn19'  
-
 end
