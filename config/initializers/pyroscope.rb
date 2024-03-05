47a743c3400e9008027bd2056c9ba4e3920b016f
Oleg Ten <tennet0505@gmail.com>
2024-03-05 20:55:19 +0100

    setup pyroscope

---
+++ config/initializers/pyroscope.rb
@@ -0,0 +1,8 @@
+require 'pyroscope'
+    
+Pyroscope.configure do |config|
+    config.application_name = "grafanacloud-plnhow-profiles"
+    config.server_address = "https://profiles-prod-001.grafana.net"
+    config.basic_auth_username='786746'
+    config.basic_auth_password='glc_eyJvIjoiOTg3NzQ5IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZXRva2Vud3JpdGVyZWFkIiwiayI6ImxDQTI0NGxORjFRclMxRzVBODhLNjJsUiIsIm0iOnsiciI6InByb2QtdXMtZWFzdC0wIn19'  
+end
