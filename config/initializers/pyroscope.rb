fb462fd502f8a8f61fe16291a9c2dda698629fe3
Oleg Ten <tennet0505@gmail.com>
2024-03-06 10:36:07 +0100

    token new

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -4,5 +4,5 @@ Pyroscope.configure do |config|
     config.application_name = 'sample-rails'
     config.server_address = 'https://profiles-prod-001.grafana.net'
     config.basic_auth_username = '786746'
-    config.basic_auth_password = 'glc_eyJvIjoiOTg3NzQ5IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZXRva2Vud3JpdGVyZWFkIiwiayI6ImxDQTI0NGxORjFRclMxRzVBODhLNjJsUiIsIm0iOnsiciI6InByb2QtdXMtZWFzdC0wIn19'
+    config.basic_auth_password = 'glc_eyJvIjoiOTg3NzQ5IiwibiI6InN0YWNrLTc4Njc0Ni1ocC1yZWFkLXB5cmlzY29wZV90b2tlbl9uZXciLCJrIjoiZDY4OXlSNE15ODl3UHY5UTh4YzVwOFBxIiwibSI6eyJyIjoicHJvZC11cy1lYXN0LTAifX0='
 end
