06bbe3f74097e976ddce5dcbac893a92df12412d
Oleg Ten <tennet0505@gmail.com>
2024-03-06 10:48:53 +0100

    change name

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -1,7 +1,7 @@
 require 'pyroscope'
     
 Pyroscope.configure do |config|
-    config.application_name = 'sample-rails'
+    config.application_name = 'lobster-app-vt93u.ondigitalocean.app'
     config.server_address = 'https://profiles-prod-001.grafana.net'
     config.basic_auth_username = '786746'
     config.basic_auth_password = 'glc_eyJvIjoiOTg3NzQ5IiwibiI6InN0YWNrLTc4Njc0Ni1ocC1yZWFkLXB5cmlzY29wZV90b2tlbl9uZXciLCJrIjoiZDY4OXlSNE15ODl3UHY5UTh4YzVwOFBxIiwibSI6eyJyIjoicHJvZC11cy1lYXN0LTAifX0='
