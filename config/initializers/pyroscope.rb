5ed1416471fc48af0a900f450f978051d205c378
Oleg Ten <tennet0505@gmail.com>
2024-03-06 13:47:13 +0100

    fix production.rb

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -3,6 +3,6 @@ require 'pyroscope'
 Pyroscope.configure do |config|
     config.application_name = "sample-rails"
     config.server_address = "https://profiles-prod-001.grafana.net"
-    config.basic_auth_username = "786746"
-    config.basic_auth_password = "glc_eyJvIjoiOTg3NzQ5IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZV90b2tlbl93cml0ZSIsImsiOiIzREVzNTJGMzVSVXVTNjJpdDBLOVVLczciLCJtIjp7InIiOiJwcm9kLXVzLWVhc3QtMCJ9fQ=="
+    config.basic_auth_username = '786746'
+    config.basic_auth_password = 'glc_eyJvIjoiOTg3NzQ5IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZV90b2tlbl93cml0ZSIsImsiOiIzREVzNTJGMzVSVXVTNjJpdDBLOVVLczciLCJtIjp7InIiOiJwcm9kLXVzLWVhc3QtMCJ9fQ=='
 end
