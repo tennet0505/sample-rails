eb0efe571a62ef8f344d37997910f006f33fadc3
Oleg Ten <tennet0505@gmail.com>
2024-03-11 19:17:15 +0100

    fix

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,21 +1,32 @@
 require 'prometheus/client'
 require 'net/http'
 
-Prometheus::Client.config do |config|
-    # Add authentication headers
-    auth_token = 'dop_v1_28acd1169ddd5b84fe7eb60cf158e25b573246274251103fb5308a7c9dbfc892'  # Replace 'YOUR_AUTH_TOKEN' with your actual authentication token
-    auth_header = "Bearer #{auth_token}"
-  
-    # Set the global HTTP headers for the Prometheus client
-    config.http do |http_config|
-      http_config.headers['Authorization'] = auth_header
-    end
-  end
+# Prometheus::Client.config do |config|
+#     auth_token = 'dop_v1_28acd1169ddd5b84fe7eb60cf158e25b573246274251103fb5308a7c9dbfc892' 
+#     config.http do |http_config|
+#       http_config.headers['Authorization'] = auth_header
+#     end
+# end
 
 module Prometheus
   module Controller
 
+    def self.authenticate_with_token(token)
+        # Implement your token authentication logic here
+        # For example, you could validate the token against a database or external service
+        # For simplicity, let's assume AuthService.verify_token(token) returns true if the token is valid
+        unless AuthService.verify_token(token)
+          raise StandardError.new('Unauthorized: Invalid token')
+        end
+      end
+
     prometheus = Prometheus::Client.registry
+
+    token = 'dop_v1_28acd1169ddd5b84fe7eb60cf158e25b573246274251103fb5308a7c9dbfc892'
+
+    # Authenticate with the obtained token
+    authenticate_with_token(token)
+
     gauge1 = Prometheus::Client::Gauge.new(:prometheus_metrics, docstring:'Health check for Prometheus Metrics')
     prometheus.register(gauge1)
 
