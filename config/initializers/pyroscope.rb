9ddde8c6de081bf9124a2808a9daffcf24be88b7
Oleg Ten <tennet0505@gmail.com>
2024-03-13 22:39:51 +0100

    test localhost

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -1,10 +1,10 @@
-if ENV['PYROSCOPE_PROFILES'] == 'enabled'
+# if ENV['PYROSCOPE_PROFILES'] == 'enabled'
     require 'pyroscope'
         
     Pyroscope.configure do |config|
         config.application_name = "sample-rails"
         config.server_address = "https://profiles-prod-001.grafana.net"
-        config.basic_auth_username = '786746'
-        config.basic_auth_password = 'glc_eyJvIjoiOTg3NzQ5IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZV90b2tlbl93cml0ZSIsImsiOiIzREVzNTJGMzVSVXVTNjJpdDBLOVVLczciLCJtIjp7InIiOiJwcm9kLXVzLWVhc3QtMCJ9fQ=='
+        config.basic_auth_username = '794348'
+        config.basic_auth_password = 'glc_eyJvIjoiOTk0MjI2IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZXRva2VuMnRlc3QiLCJrIjoiZ3U3OWVESTA3Nlk4ZTM1OWZ2bEEwYnFqIiwibSI6eyJyIjoidXMifX0='
     end
-end
+# end
