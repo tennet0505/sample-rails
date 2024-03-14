578162476c5206073df069a0ca08ba374e32c501
Oleg Ten <tennet0505@gmail.com>
2024-03-14 11:51:19 +0100

    develope mode

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -1,4 +1,4 @@
-# if ENV['PYROSCOPE_PROFILES'] == 'enabled'
+if ENV['PYROSCOPE_PROFILES'] == 'enabled'
     require 'pyroscope'
         
     Pyroscope.configure do |config|
@@ -7,4 +7,4 @@
         config.basic_auth_username = '794348'
         config.basic_auth_password = 'glc_eyJvIjoiOTk0MjI2IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZXRva2VuMnRlc3QiLCJrIjoiZ3U3OWVESTA3Nlk4ZTM1OWZ2bEEwYnFqIiwibSI6eyJyIjoidXMifX0='
     end
-# end
+end
