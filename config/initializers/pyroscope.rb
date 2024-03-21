a2cda5b45dfc2582b6ddeb979b676f110ff89129
Oleg Ten <tennet0505@gmail.com>
2024-03-21 17:16:06 +0100

    add telemetry

--- config/initializers/pyroscope.rb
+++ config/initializers/pyroscope.rb
@@ -1,6 +1,7 @@
 if ENV['PYROSCOPE_PROFILES'] == 'enabled'
     require 'pyroscope'
-        
+    require 'pyroscope/otel'
+
     if ENV['IS_PRODUCTION_CRED'] == 'true' 
         Pyroscope.configure do |config|
             config.application_name = "sample-rails"
@@ -15,5 +16,13 @@ if ENV['PYROSCOPE_PROFILES'] == 'enabled'
             config.basic_auth_username = '794348'
             config.basic_auth_password = 'glc_eyJvIjoiOTk0MjI2IiwibiI6InB5cm9zY29wZXRva2VuLXB5cm9zY29wZXRva2VuMnRlc3QiLCJrIjoiZ3U3OWVESTA3Nlk4ZTM1OWZ2bEEwYnFqIiwibSI6eyJyIjoidXMifX0='
         end
+    end
+
+    OpenTelemetry::SDK.configure do |c|
+        c.service_name = app_name
+        c.add_span_processor Pyroscope::Otel::SpanProcessor.new("#{app_name}.cpu", pyroscope_server_address)
+        c.add_span_processor OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+          OpenTelemetry::Exporter::Jaeger::CollectorExporter.new(endpoint: jaeger_endpoint))
+        c.use_all()
     end
 end
